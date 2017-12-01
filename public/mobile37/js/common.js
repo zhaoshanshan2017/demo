27304d0db1e2b1268ff3f10310546223f3c8d0a8
zhousg <zhoushugang@itcast.cn>
2017-12-01 11:29:03 +0800

    day02

--- public/mobile37/js/common.js
+++ public/mobile37/js/common.js
@@ -23,4 +23,45 @@ lt.getParamsByUrl = function () {
         });
     }
     return params;
-}
+};
+/*
+* 拦截ajax请求未登录状态
+* 具备ajax功能同做未登录的判断处理
+* */
+lt.LOGINURL = '/mobile37/user/login.html';
+lt.USERURL = '/mobile37/user/index.html';
+lt.CARTURL = '/mobile37/user/cart.html';
+lt.ajax = function (options) {
+    /* options 和原理的ajax传参保持一致*/
+    $.ajax({
+        type:options.type||'get',
+        url:options.url || '#',
+        data:options.data || '',
+        dataType:options.dataType || 'json',
+        success:function (data) {
+            /*如果 data.error 存在且等于400 认为未登录*/
+            if(data.error == 400){
+                location.href = lt.LOGINURL + '?returnUrl=' + encodeURIComponent(location.href);
+            }
+            /*登录成功*/
+            else{
+                options.success && options.success(data);
+            }
+        },
+        error:function (info) {
+            options.error && options.error(info);
+        }
+    });
+};
+/*转换表单序列化的数据为一个对象*/
+lt.serialize2object = function (serialize) {
+    var obj = {};
+    if(serialize){
+        var serializeArr = serialize.split('&');
+        serializeArr.forEach(function (item,i) {
+            var itemArr = item.split('=');
+            obj[itemArr[0]] = itemArr[1];
+        });
+    }
+    return obj;
+};
